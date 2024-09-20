227b9dac41fc88f8fea43d608f49b69af71f25fd
Edwin Oswaldo Flores Gutierrez <46845817+EdwinFlores29@users.noreply.github.com>
2024-09-20 09:32:03 -0600

    modificar views
    se hizo la modificación de views para tener una api aparte para solo modificar el estado booleano de true o false, a pedir método POST.

--- tasks/views.py
+++ tasks/views.py
@@ -1,9 +1,21 @@
 from rest_framework import viewsets, permissions
 from .models import Tasks
 from .serializers import TasksSerializer
+from rest_framework.decorators import action
+from rest_framework.response import Response
+from rest_framework import status
 
 
 class TasksViewSet(viewsets.ModelViewSet):
     queryset = Tasks.objects.all()
     permission_classes = [permissions.AllowAny]
     serializer_class = TasksSerializer
+
+    @action(detail=True, methods=['POST'])
+    def done(self, request, pk=None):
+        task = self.get_object()
+        task.done = not task.done
+        task.save()
+        return Response({
+            'status': 'task done' if task.done else 'task failed'
+        }, status=status.HTTP_200_OK)
